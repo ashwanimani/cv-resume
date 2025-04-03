f8af21667c44a76e1d3cd04071cb9d705bf6f378
ashwanitripathi421 <ashwanitripathi421@gmail.com>
2025-04-03 06:37:46 +0000

    Added placeholder content to Main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,48 +1,45 @@
-// src/components/Main.jsx
+import React from 'react';
+
 function Main() {
   return (
     <div id="main">
       <div>
         <h2>About</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-          risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
-          ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
-          massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci
-          nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit
-          amet erat.
-        </p>
+        <p>I am a highly motivated and detail-oriented software engineer with experience in developing web applications. I am passionate about creating high-quality, user-friendly software and am always eager to learn new technologies.</p>
       </div>
       <div>
         <h2>Experience</h2>
-        <p>
-          Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium
-          a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra
-          tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.
-          Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit
-          sodales. Vestibulum ante ipsum primis in faucibus orci luctus et
-          ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede
-          pellentesque fermentum.
-        </p>
+        <ul>
+          <li>
+            <strong>Software Engineer</strong> - Company A (2020 - Present)
+            <ul>
+              <li>Developed and maintained web applications.</li>
+              <li>Collaborated with cross-functional teams to deliver high-quality software.</li>
+            </ul>
+          </li>
+          <li>
+            <strong>Junior Developer</strong> - Company B (2018 - 2020)
+            <ul>
+              <li>Assisted senior developers in coding and debugging tasks.</li>
+              <li>Contributed to the development of new features.</li>
+            </ul>
+          </li>
+        </ul>
       </div>
       <div>
         <h2>Education</h2>
-        <p>
-          Maecenas sollicitudin. In auctor lobortis lacus. Quisque libero metus,
-          condimentum nec, tempor a, commodo mollis, magna. Vestibulum
-          ullamcorper mauris at ligula. Fusce fermentum. Nullam mi. Sed aliquam
-          augue ut lacus. Morbi leo risus, porta ac, consectetur ac, vestibulum
-          at, eros.
-        </p>
+        <p><strong>Bachelor of Science in Computer Science</strong> - University X (2014 - 2018)</p>
       </div>
       <div>
         <h2>Skills</h2>
-        <p>
-          Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum
-          primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-          Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum.
-          Maecenas adipiscing ante non diam sodales hendrerit.
-        </p>
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Git</li>
+        </ul>
       </div>
     </div>
   );
