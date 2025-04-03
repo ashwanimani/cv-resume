973c1279f214c54a62282cd1612a6bc23426619d
ashwanitripathi421 <ashwanitripathi421@gmail.com>
2025-04-03 06:36:54 +0000

    Added basic styling to Header and Main components

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+// src/components/Header.jsx
+// src/components/Header.jsx
+import './Header.css';
+function Header() {
+    return (
+      <header>
+        <h1>My CV Resume</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
