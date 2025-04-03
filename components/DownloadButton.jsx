a3569f24f0dfc2ef6d7cc5a27f479aae2fbe3de9
ashwanitripathi421 <ashwanitripathi421@gmail.com>
2025-04-03 06:45:19 +0000

    Added styles for download-button and fixed jspdf callback

--- components/DownloadButton.jsx
+++ components/DownloadButton.jsx
@@ -4,16 +4,14 @@ import { jsPDF } from 'jspdf';
 function DownloadButton() {
   const handleDownload = () => {
     const doc = new jsPDF();
-    const element = document.body;
-
-    doc.html(element, {
+    doc.html(document.body, {
       callback: (pdf) => {
         pdf.save('resume.pdf');
-      },
+      }, 
       margin: [10, 10, 10, 10],
       autoPaging: 'text',
       x: 0,
-      y: 0,
+      y: 0, 
       width: 190,
       windowWidth: 675
     });
