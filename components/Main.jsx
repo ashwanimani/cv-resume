8e30fa68b65500fb78108f668b3187e3cb316344
ashwanitripathi421 <ashwanitripathi421@gmail.com>
2025-04-03 06:43:18 +0000

    Added provided sample content to Main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,48 +1,58 @@
 import React from 'react';
 
 function Main() {
-  return (
-    <div id="main">
-      <div>
-        <h2>About</h2>
-        <p>I am a highly motivated and detail-oriented software engineer with experience in developing web applications. I am passionate about creating high-quality, user-friendly software and am always eager to learn new technologies.</p>
-      </div>
-      <div>
-        <h2>Experience</h2>
-        <ul>
-          <li>
-            <strong>Software Engineer</strong> - Company A (2020 - Present)
-            <ul>
-              <li>Developed and maintained web applications.</li>
-              <li>Collaborated with cross-functional teams to deliver high-quality software.</li>
-            </ul>
-          </li>
-          <li>
-            <strong>Junior Developer</strong> - Company B (2018 - 2020)
-            <ul>
-              <li>Assisted senior developers in coding and debugging tasks.</li>
-              <li>Contributed to the development of new features.</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2>Education</h2>
-        <p><strong>Bachelor of Science in Computer Science</strong> - University X (2014 - 2018)</p>
-      </div>
-      <div>
-        <h2>Skills</h2>
-        <ul>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Git</li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div id="main">
+            <div>
+                <h1>RITESH SINGH</h1>
+                <p>Engineering student</p>
+            </div>
+            <div>
+                <h2>PROFILE</h2>
+                <p>Engineering student. I consider myself a responsible and orderly person. I am looking forward to my first work experience in most reputed company where I can get more knowledge.</p>
+            </div>
+            <div>
+                <h2>CONTACT ME</h2>
+                <ul>
+                    <li>8229078841</li>
+                    <li>Singhrtes.1701@gmail.com</li>
+                    <li>Kishunpur, Bihta, Patna</li>
+                </ul>
+            </div>
+            <div>
+                <h2>EDUCATION</h2>
+                <p>SBTE</p>
+                <p>Completed my diploma in Computer science engineering from Government Polytechnic Patna-7</p>
+                <p>SVIET(IKJPTU) 2022-2026</p>
+                <p>Pursuing B.tech of more knowledge in my interest computer science</p>
+            </div>
+            <div>
+                <h2>LANGUAGE</h2>
+                <ul>
+                    <li>English</li>
+                    <li>Hindi</li>
+                </ul>
+            </div>
+            <div>
+                <h2>COMPUTER SKILLS</h2>
+                <ul>
+                    <li>Programming in python</li>
+                    <li>Java</li>
+                    <li>C Programming</li>
+                    <li>Graphic design</li>
+                    <li>Web development</li>
+                </ul>
+            </div>
+            <div>
+                <h2>EXPERIENCE</h2>
+                <p>ICE TECHNOLOGY LAB</p>
+                <ul>
+                    <li>Web development (4 weeks)</li>
+                    <li>Java programming (6 weeks)</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Main;
